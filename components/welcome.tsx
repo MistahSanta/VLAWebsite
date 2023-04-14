95b0604b0c29eddc59aba054a91c9552fb68349f
Jonathan Le <jonathanle1111@gmail.com>
2023-04-13 21:49:35 -0500

    first commit

---
+++ components/welcome.tsx
@@ -0,0 +1,24 @@
+
+export default function Welcome() {
+  return (
+    <>
+      <div
+        id="home"
+        className="flex flex-col gradient-pink-green
+        h-screen w-screen border-b-4 border-white"
+      >
+    
+        <div className="flex flex-col h-full w-full justify-center items-center">
+          <p className=" italic font-serif text-7xl text-black z-10">
+            Welcome to {"\n"}
+          </p>
+          <p className="z-10 font-bold font-sans text-7xl text-black text-center">
+            Vietnamese Language Association 
+          </p>
+
+
+        </div>
+      </div>
+    </>
+  );
+}
