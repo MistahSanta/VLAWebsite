d6c6f9c60dd0a149cbfa61291cfdf79f487ac2b8
Jonathan Le <jonathanle1111@gmail.com>
2023-04-14 12:18:46 -0500

    fixed style

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -7,7 +7,7 @@ export default function Welcome() {
         className="flex flex-col gradient-pink-green min-h-screen min-w-full shadow-lg shadow-amber-300"
       >
       <div className="gradient-white min-h-screen mt-1">
-        <div className="flex flex-row mx-auto items-center max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-4xl 2xl:max-w-6xl py-10 sm:py-20 md:pt-30 lg:py-40 justify-center">
+        <div className="flex flex-row mx-auto items-center max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-4xl 2xl:max-w-6xl py-10 sm:py-20 md:pt-30 justify-center">
         <img src="./vietWave.png" className="w-40 h-40"></img>
           <p className="text-center font-bold italic font-serif text-7xl text-black z-10">
           Xin Chào! {"\n"}
@@ -18,7 +18,7 @@ export default function Welcome() {
         <p className="font-sans font-bold text-7xl text-black text-center">
             We are <span className="text-9xl">VLA</span>
         </p>
-      <p className="font-sans text-4xl text-black text-center pt-6">Our mission is to teach Vietnamese and have fun while doing it!</p>
+      <p className="font-sans text-4xl text-gray-900 text-center pt-6">Our mission is to teach Vietnamese and have fun while doing it!</p>
       </div>
       </div>
     </>
